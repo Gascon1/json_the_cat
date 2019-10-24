8398090ffd49c9428ca30e709aff9f63fb8f2659
Mikel Gascon <mikelgascon@outlook.com>
2019-10-24 18:10:33 +0000

    used ESlint to fix linting mistakes

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,22 @@
 const request = require('request');
 let userInput = process.argv.slice(2);
-const baseSearch = 'https://api.thecatapi.com/v1/breeds/search?q=' + userInput
+const URL = 'https://api.thecatapi.com/v1/breeds/search?q=' + userInput;
 
 
 
 
-console.log(`Looking for ${userInput}...`)
-request(baseSearch, (error, response, body) => {
-  const data = JSON.parse(body)
-  if (data[0] !== undefined){
-    console.log(data[0]['description'])
+console.log(`Looking for ${userInput}...`);
+request(URL, (error, response, body) => {
+  if (error) {
+    console.log('Something seems to be wrong with the URL');
   } else {
-    console.log('Seems like you didnt write a valid cat breed!')
+    const data = JSON.parse(body);
+    if (data[0] !== undefined) {
+      console.log(data[0]['description']);
+    } else {
+      console.log('Seems like you didnt write a valid cat breed!');
+    }
   }
-  
 });
 
 
